fca0ce018084949860012ff2a87ce8c7ca67200e
Ana Belen Moreno Rojas <al222310526@gmail.com>
2026-03-13 14:46:53 -0600

    Estilo completo tienda UTVT

--- app.js
+++ app.js
@@ -42,3 +42,15 @@ welcome.textContent = "Hola, " + loggedUser
 }
 
 }
+
+let user = localStorage.getItem("loggedUser")
+
+if(user){
+
+let welcome = document.getElementById("welcome")
+
+if(welcome){
+welcome.textContent = "Hola, " + user
+}
+
+}
